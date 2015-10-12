8a79256ad94231ae32257240565dcb304f3c8855
Wanghuixing <wanghuixing@baidu.com>
2015-10-12 21:50:25 +0800

    [Fix Crash]

--- PaperInSwift/CHNavigationViewController.swift
+++ PaperInSwift/CHNavigationViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 
 public class CHNavigationViewController : UINavigationController, UINavigationControllerDelegate, CHControllerTransitionDelegate {
     private var transitionController : CHTranstionController?
-    required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+//    required public init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//        fatalError("init(coder:) has not been implemented")
+//    }
     
     override public func viewDidLoad() {
         super.viewDidLoad()
