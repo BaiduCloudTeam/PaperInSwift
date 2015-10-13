a72314e105488fdf7c52aad7c41a05e98a73c3dd
Wanghuixing <wanghuixing@baidu.com>
2015-10-13 15:04:56 +0800

    [Implement startInteractiveTransition method]

--- PaperInSwift/CHTransitionViewController.swift
+++ PaperInSwift/CHTransitionViewController.swift
@@ -22,6 +22,7 @@ public class CHTranstionController: NSObject, UIViewControllerAnimatedTransition
     
     // MARK: - Private Properties
     private var transitionLayout : CHTransitionLayout?
+    private weak var context : UIViewControllerContextTransitioning?
     private var initialPinchDistance : CGFloat?
     private var initialPinchPoint : CGPoint?
     private var initialScale : CGFloat?
@@ -45,7 +46,21 @@ public class CHTranstionController: NSObject, UIViewControllerAnimatedTransition
     
     // MARK: - UIViewControllerInteractiveTransitioning Delegate Method
     public func startInteractiveTransition(transitionContext: UIViewControllerContextTransitioning) {
+        context = transitionContext;
         
+        let fromCollectionViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey) as? UICollectionViewController
+    
+        let toCollectionViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) as? UICollectionViewController
+        
+        let containerView = transitionContext.containerView()
+        containerView?.addSubview((toCollectionViewController?.view)!)
+        
+        fromCollectionViewController?.collectionView?.startInteractiveTransitionToCollectionViewLayout((toCollectionViewController?.collectionViewLayout)!, completion: {didFinish,didComplete in
+            self.context?.completeTransition(didComplete)
+            self.transitionLayout = nil
+            self.context = nil
+            self.hasActiveInteraction = false
+        })
     }
     
     // MARK: - CHControllerTransitionDelegate Method
@@ -55,6 +70,10 @@ public class CHTranstionController: NSObject, UIViewControllerAnimatedTransition
     
     func handlePinch(pinchGesture : UIPinchGestureRecognizer) {
         
+    }
+    
+    public func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
     
 }
